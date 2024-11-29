83b0faa2147ec7cccf02d6c145d497882b4268d7
qrh <652440522@qq.com>
2024-11-30 00:14:10 +0800

    Build64

---
+++ Build64/CPackSourceConfig.cmake
@@ -0,0 +1,72 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BUILD_SOURCE_DIRS "D:/proj/IBLBaker;D:/proj/IBLBaker/Build64")
+set(CPACK_CMAKE_GENERATOR "Visual Studio 16 2019")
+set(CPACK_COMPONENTS_ALL "Unspecified;assimp-dev;libassimp3.2.0-dev")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "C:/Program Files/CMake316/share/cmake-3.30/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "IBLBaker built using CMake")
+set(CPACK_DMG_SLA_USE_RESOURCE_FILE_LICENSE "ON")
+set(CPACK_GENERATOR "TGZ")
+set(CPACK_IGNORE_FILES ".a$;.cmake$;.o$;.so$;/.deps/;/.libs/;/CMakeFiles/;/CVS/;/autom4te.cache/;/build;/broken;/docs/;AdditionalInfo.txt$;CMakeCache.txt$;Makefile$;\\.sh$;_CPack_;config.h$;config.log$;config.status$;html$;install_manifest.txt$;stamp-h1$;tar.bz2;tar.gz;~$")
+set(CPACK_INNOSETUP_ARCHITECTURE "x64")
+set(CPACK_INSTALLED_DIRECTORIES "D:/proj/IBLBaker;/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "")
+set(CPACK_INSTALL_PREFIX "C:/Program Files/IBLBaker")
+set(CPACK_MODULE_PATH "")
+set(CPACK_NSIS_DISPLAY_NAME "IBLBaker 0.11.2")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+set(CPACK_NSIS_PACKAGE_NAME "IBLBaker 0.11.2")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OUTPUT_CONFIG_FILE "D:/proj/IBLBaker/Build64/CPackConfig.cmake")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "D:/proj/IBLBaker/src/critter/dependencies/libzip/README")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "library for manipulating zip archives")
+set(CPACK_PACKAGE_FILE_NAME "libzip-0.11.2")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "IBLBaker 0.11.2")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "IBLBaker 0.11.2")
+set(CPACK_PACKAGE_NAME "IBLBaker")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "Dieter Baron <dillo@giga.or.at> and Thomas Klausner <tk@giga.or.at>")
+set(CPACK_PACKAGE_VERSION "0.11.2")
+set(CPACK_PACKAGE_VERSION_MAJOR "0")
+set(CPACK_PACKAGE_VERSION_MINOR "11")
+set(CPACK_PACKAGE_VERSION_PATCH "2")
+set(CPACK_RESOURCE_FILE_LICENSE "D:/proj/IBLBaker/src/critter/dependencies/libzip/LICENSE")
+set(CPACK_RESOURCE_FILE_README "C:/Program Files/CMake316/share/cmake-3.30/Templates/CPack.GenericDescription.txt")
+set(CPACK_RESOURCE_FILE_WELCOME "C:/Program Files/CMake316/share/cmake-3.30/Templates/CPack.GenericWelcome.txt")
+set(CPACK_RPM_PACKAGE_SOURCES "ON")
+set(CPACK_SET_DESTDIR "OFF")
+set(CPACK_SOURCE_GENERATOR "TGZ")
+set(CPACK_SOURCE_IGNORE_FILES ".a$;.cmake$;.o$;.so$;/.deps/;/.libs/;/CMakeFiles/;/CVS/;/autom4te.cache/;/build;/broken;/docs/;AdditionalInfo.txt$;CMakeCache.txt$;Makefile$;\\.sh$;_CPack_;config.h$;config.log$;config.status$;html$;install_manifest.txt$;stamp-h1$;tar.bz2;tar.gz;~$")
+set(CPACK_SOURCE_INSTALLED_DIRECTORIES "D:/proj/IBLBaker;/")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "D:/proj/IBLBaker/Build64/CPackSourceConfig.cmake")
+set(CPACK_SOURCE_PACKAGE_FILE_NAME "libzip-0.11.2")
+set(CPACK_SOURCE_STRIP_FILES "")
+set(CPACK_SOURCE_TOPLEVEL_TAG "win64-Source")
+set(CPACK_STRIP_FILES "")
+set(CPACK_SYSTEM_NAME "win64")
+set(CPACK_THREADS "1")
+set(CPACK_TOPLEVEL_TAG "win64-Source")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "D:/proj/IBLBaker/Build64/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
